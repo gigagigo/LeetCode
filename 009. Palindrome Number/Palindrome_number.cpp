0a226e6c3df060b5ded58f65190191e3f65f706f
gigagigo <47029490+gigagigo@users.noreply.github.com>
2019-03-28 17:24:38 +0900

    Add files via upload

---
+++ 009. Palindrome Number/Palindrome_number.cpp
@@ -0,0 +1,20 @@
+﻿class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        if (x == 0) return true;
+        
+        vector<int> parse;
+        
+        while (x > 0){
+            parse.push_back(x%10);
+            x = x / 10;
+        }
+        
+        for (int i=0; i <= parse.size()/2; i++){
+            if (parse[i] != parse[parse.size()-i-1]) {return false;}
+        }
+        
+        return true;
+    }
+};
---
+++ 009. Palindrome Number/Palindrome_number_using_to_string.cpp
@@ -0,0 +1,10 @@
+﻿class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        string s = to_string(x);
+        string r = s;
+        reverse(r.begin(), r.end());
+        return !s.compare(r);
+    }
+};
